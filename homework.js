3c3df88cbdebb36e273fcc16ca09c9c58c43a57d
KarenElaine <karenelaine.stewart@yahoo.com>
2022-02-02 20:49:39 -0500

    removed array, preserved and added sum

--- homework.js
+++ homework.js
@@ -6,14 +6,83 @@
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
 
+// This function takes two parameters: an array and a value
+// to remove from the array
+
+function remove(array, arrayValue) {
+        console.log('Length of this array is ', array.length)
+        let keepArray = []
+        for (let index = 0; index < array.length; index++) 
+        {
+            if (array[index] !== arrayValue) {
+            console.log( 'Keep ' +array[index])
+            keepArray.push(array[index])
+            console.log(index, 'index') 
+        }
+    }
+    return keepArray
+}
+// Here we are asking JavaScript to run our function
+// called remove. When the function runs, [1,2,3] as array
+// and 1 as arrayValue. 
+console.log (remove(['1', '2', '3'], '1'));
+
+// When JavaScript runs the fuction array becomes 
+// [1, 2, 3] and arrayValue becomes 1
+
+// function remove([1,2,3], 1) {
+//     {
+        // here we are asking the function to output to the console
+        // a string and the length of the array which is now
+        // [1,2,3]
+//         console.log('Length of this array is ' + [1, 2, 3].length)
+
+        // here we are looping over [1,2,3] and using its length
+        // to determine how many times the loop will run
+//         for (let index = 0; index < [1, 2, 3].length; index++) {
+    
+                // here we are asking the function to output 
+                // to the console the value of the array
+                // according to the value's index position
+//             console.log([1, 2, 3][index])
+//         }
+        // here we are returning the original array
+        // that  was passed in as an argument when we
+        // asked JavaScript to run the function
+//     } return [1, 2, 3]
+// }
+//
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
+
+
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
 
+function sum(input){
+             
+    if (toString.call(input) !== "[object Array]")
+       return false;
+         
+               var total =  0;
+               for(var i=0;i<input.length;i++)
+                 {                  
+                   if(isNaN(input[i])){
+                   continue;
+                    }
+                     total += Number(input[i]);
+                  }
+                return total;
+               }
+   console.log(sum([1,2,3]));
+   console.log(sum([100,-200,3]));
+   console.log(sum([1,2,'a',3]));
+
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+
+
 
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
